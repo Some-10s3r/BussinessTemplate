958be3d0c0f6ef0e9179dd5bd4ac8e7f991ef8ee
Some-10s3r <some.10s3r.123@gmail.com>
2023-04-21 10:23:12 -1100

    f

---
+++ js/hamb.js
@@ -0,0 +1,5 @@
+const hambFunc=()=>{
+    document.querySelector('.navbar__toggle').classList.toggle('is-active');
+    document.querySelector('.hamb-links').classList.toggle('active-links');
+    document.querySelector('.hamb-bg').classList.toggle('active-bg');
+}
